9a822909ee40c69ee7b9b9a0a3707e2729365709
AZANK7173 <felipeazank@gmail.com>
2024-04-21 01:52:18 +0200

    add layout

--- fodase.py
+++ fodase.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 
+st.write("page x de fodase")
 
-# Create two columns
-col1, col2 = st.columns(2)
-
-# Input fields for the first column
 pdf_viewer("CS.pdf",width=700, height=500, pages_to_render=[1])
 
-# Input fields for the second column
-button = col2.button('fodase?')
+st.button("Previous")
+st.button("Next")
 
+st.write("summary")
+
+st.write("ask me about it")
